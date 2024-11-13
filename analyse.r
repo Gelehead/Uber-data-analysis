e1d67160ebe2c70779142f59fc5eb49ce9443957
Gelehead <oscar.lavolet@gmail.com>
2024-11-13 02:24:38 +0100

    first commit

---
+++ analyse.r
@@ -0,0 +1,8 @@
+filenames = [
+    "uber-raw-data-apr14.csv",
+    "uber-raw-data-aug14.csv",
+    "uber-raw-data-jul14.csv",
+    "uber-raw-data-jun14.csv",
+    "uber-raw-data-may14.csv",
+    "uber-raw-data-sep14.csv",
+]
